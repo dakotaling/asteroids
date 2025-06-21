0de5d94b136bfdef32e3421eb9f2d3dcc8bc4880
dakotal709 <dakota.ling@marquette.edu>
2025-06-20 23:13:06 -0400

    Added ability to shoot out of player model

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -31,7 +32,14 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
             
+    # Add instance of Shot
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
