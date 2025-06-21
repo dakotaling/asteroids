5a40a33ece33701e8c0843a25af2d5877bdbcbf4
dakotal709 <dakota.ling@marquette.edu>
2025-06-20 22:03:53 -0400

    Adding rotational movement

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player(CircleShape):
     # override draw helper function
     def draw(self, screen):
         return super().draw(pygame.draw.polygon(screen, "white", self.triangle(), 2))
-    
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,3 +18,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    # Add movement use delta time    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+            
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
