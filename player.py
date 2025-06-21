8e81ec76dc423c4cf89d7bde8055d25595628bd6
dakotal709 <dakota.ling@marquette.edu>
2025-06-20 23:32:02 -0400

    Adding rate limit to shooting

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     # override draw helper function
     def draw(self, screen):
@@ -22,6 +23,7 @@ class Player(CircleShape):
     
     # Add movement use delta time    
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
@@ -37,6 +39,9 @@ class Player(CircleShape):
             
     # Add instance of Shot
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
